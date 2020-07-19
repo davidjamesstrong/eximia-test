b9081e7b86a1e3f13401147bf88da7a9e1f8e487
Dave Strong <davidjamesstrong@outlook.com>
2020-07-19 19:21:41 +0100

    store subscription

---
+++ app/Http/Controllers/SubscribersController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Subscriber;
+use Illuminate\Http\Request;
+
+class SubscribersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('subscribe');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $subscriber = new Subscriber;
+        $subscriber->first_name = request('first_name');
+        $subscriber->last_name = request('last_name');
+        $subscriber->email_address = request('email_address');
+        $subscriber->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subscriber $subscriber)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Subscriber  $subscriber
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subscriber $subscriber)
+    {
+        //
+    }
+}
