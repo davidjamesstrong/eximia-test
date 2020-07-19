5f796903249936627a8279cabc738b0103f1d23b
Dave Strong <davidjamesstrong@outlook.com>
2020-07-19 16:08:44 +0100

    Create subscribers migration

---
+++ database/migrations/2020_07_19_145631_create_subscribers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscribersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('first_name', 100);
+            $table->char('last_name', 100);
+            $table->char('email_address', 255);
+            $table->timestamp('verified_on', 0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscribers');
+    }
+}
